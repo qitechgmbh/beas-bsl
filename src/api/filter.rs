76737cb05729520daaece8963e6aad1267a3f6ae
just_some_entity <entity@jsentity.dev>
2026-02-12 23:33:55 +0100

    filter update

--- src/api/filter.rs
+++ src/api/filter.rs
@@ -5,22 +5,28 @@ pub struct Filter
 }
 
 #[derive(Debug, Clone)]
+pub struct FilterBuilder
+{
+    pub data: String
+}
+
+#[derive(Debug, Clone)]
 pub struct OperatorSelector
 {
-    filter: Filter
+    builder: FilterBuilder
 }
 
 #[derive(Debug, Clone)]
 pub struct LogicalOperatorSelector
 {
-    filter: Filter
+    builder: FilterBuilder
 }
 
-impl Filter
+impl FilterBuilder
 {
     pub fn new() -> OperatorSelector
     {
-        OperatorSelector { filter: Self { data: String::new() } }
+        OperatorSelector { builder: Self { data: String::new() } }
     }
 }
 
@@ -29,49 +35,49 @@ impl OperatorSelector
     pub fn equals<T: ToValue>(mut self, field: &str, value: T) -> LogicalOperatorSelector
     {
         let string = format!("{} eq {}", field, value.convert());
-        self.filter.data.push_str(&string);
+        self.builder.data.push_str(&string);
 
-        LogicalOperatorSelector { filter: self.filter }
+        LogicalOperatorSelector { builder: self.builder }
     }
 
     pub fn not_equals<T: ToValue>(mut self, field: &str, value: T) -> LogicalOperatorSelector
     {
         let string = format!("{} ne {}", field, value.convert());
-        self.filter.data.push_str(&string);
+        self.builder.data.push_str(&string);
 
-        LogicalOperatorSelector { filter: self.filter }
+        LogicalOperatorSelector { builder: self.builder }
     }
 
     pub fn greater_than<T: ToValue>(mut self, field: &str, value: T) -> LogicalOperatorSelector
     {
         let string = format!("{} gt {}", field, value.convert());
-        self.filter.data.push_str(&string);
+        self.builder.data.push_str(&string);
 
-        LogicalOperatorSelector { filter: self.filter }
+        LogicalOperatorSelector { builder: self.builder }
     }
 
     pub fn greater_than_or_equal<T: ToValue>(mut self, field: &str, value: T) -> LogicalOperatorSelector
     {
         let string = format!("{} ge {}", field, value.convert());
-        self.filter.data.push_str(&string);
+        self.builder.data.push_str(&string);
 
-        LogicalOperatorSelector { filter: self.filter }
+        LogicalOperatorSelector { builder: self.builder }
     }
 
     pub fn less_than<T: ToValue>(mut self, field: &str, value: T) -> LogicalOperatorSelector
     {
         let string = format!("{} lt {}", field, value.convert());
-        self.filter.data.push_str(&string);
+        self.builder.data.push_str(&string);
 
-        LogicalOperatorSelector { filter: self.filter }
+        LogicalOperatorSelector { builder: self.builder }
     }
 
     pub fn less_than_or_equal<T: ToValue>(mut self, field: &str, value: T) -> LogicalOperatorSelector
     {
         let string = format!("{} le {}", field, value.convert());
-        self.filter.data.push_str(&string);
+        self.builder.data.push_str(&string);
 
-        LogicalOperatorSelector { filter: self.filter }
+        LogicalOperatorSelector { builder: self.builder }
     }
 
     // non ODATA compliant beas-bsl string operators
@@ -79,35 +85,27 @@ impl OperatorSelector
     {
         let escaped = value.replace('\'', "''");
         let string = format!("contains({}, '{}')", field, escaped);
-        self.filter.data.push_str(string.as_str());
+        self.builder.data.push_str(string.as_str());
         
-        LogicalOperatorSelector { filter: self.filter }
+        LogicalOperatorSelector { builder: self.builder }
     }
 
     pub fn starts_with(mut self, field: &str, value: &str) -> LogicalOperatorSelector
     {
         let escaped = value.replace('\'', "''");
         let string = format!("startswith({}, '{}')", field, escaped);
-        self.filter.data.push_str(string.as_str());
+        self.builder.data.push_str(string.as_str());
         
-        LogicalOperatorSelector { filter: self.filter }
+        LogicalOperatorSelector { builder: self.builder }
     }
 
     pub fn ends_with(mut self, field: &str, value: &str) -> LogicalOperatorSelector
     {
         let escaped = value.replace('\'', "''");
         let string = format!("endswith({}, '{}')", field, escaped);
-        self.filter.data.push_str(string.as_str());
+        self.builder.data.push_str(string.as_str());
         
-        LogicalOperatorSelector { filter: self.filter }
-    }
-}
-
-impl From<OperatorSelector> for Filter
-{
-    fn from(selector: OperatorSelector) -> Self
-    {
-        selector.filter
+        LogicalOperatorSelector { builder: self.builder }
     }
 }
 
@@ -115,25 +113,21 @@ impl LogicalOperatorSelector
 {
     pub fn and(mut self) -> OperatorSelector
     {
-        self.filter.data.push_str(" and ");
-        return OperatorSelector { filter: self.filter }
+        self.builder.data.push_str(" and ");
+        return OperatorSelector { builder: self.builder }
     }
 
     pub fn or(mut self) -> OperatorSelector
     {
-        self.filter.data.push_str(" or ");
-        return OperatorSelector { filter: self.filter }
+        self.builder.data.push_str(" or ");
+        return OperatorSelector { builder: self.builder }
     }
-}
-
-impl From<LogicalOperatorSelector> for Filter
-{
-    fn from(selector: LogicalOperatorSelector) -> Self
+    
+    pub fn build(self) -> Filter
     {
-        selector.filter
+        Filter { data: self.builder.data }
     }
 }
-
 
 pub trait ToValue
 {
