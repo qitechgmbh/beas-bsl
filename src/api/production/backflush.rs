9c06d8a46109d1f1f8fd7c756dd41ca9981cadfd
just-some-entity <entity@jsentity.dev>
2026-03-04 18:59:54 +0100

    make backflush fields public and add windows install script

--- src/api/production/backflush.rs
+++ src/api/production/backflush.rs
@@ -5,29 +5,29 @@ pub struct BackflushRequest
 {
     /// Work oder number
     #[serde(rename = "DocEntry")]
-    doc_entry: u32,
+    pub doc_entry: u32,
     
     /// Line Number
     #[serde(rename = "LineNumber")]
-    line_number: u32,
+    pub line_number: u32,
     
     /// Optional Document date, if not today.
     #[serde(rename = "DocDate")]
-    doc_date: Option<f32>,
+    pub doc_date: Option<f32>,
     
     /// set to true, if you want to close the work order
     #[serde(rename = "CloseEntry")]
-    close_entry: bool,
+    pub close_entry: bool,
     
     /// Good Quantity in Warehouse Unit
     #[serde(rename = "QuantityGood")]
-    quantity_good: u32,
+    pub quantity_good: u32,
     
     /// Issue information
     #[serde(rename = "IssueLines")]
-    issue_lines: Vec<String>,
+    pub issue_lines: Vec<String>,
     
     /// receipt information
     #[serde(rename = "ReceiptLines")]
-    receipt_lines: Vec<String>,
+    pub receipt_lines: Vec<String>,
 }
