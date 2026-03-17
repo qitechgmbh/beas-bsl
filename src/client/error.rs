d8216f8f036a84255ca0741f0d475b29ccfdc643
jsentity <entity@jsentity.dev>
2026-03-17 05:12:47 +0100

    formatting

--- src/client/error.rs
+++ src/client/error.rs
@@ -1,48 +1,38 @@
-use std::fmt;
 use crate::api;
+use std::fmt;
 
 #[derive(Debug)]
-pub enum TransactionError
-{
+pub enum TransactionError {
     UreqError(ureq::Error),
     JsonError(serde_json::Error),
     IoError(std::io::Error),
     ApiError(api::Error),
 }
 
-impl From<ureq::Error> for TransactionError 
-{
-    fn from(err: ureq::Error) -> Self 
-    {
+impl From<ureq::Error> for TransactionError {
+    fn from(err: ureq::Error) -> Self {
         TransactionError::UreqError(err)
     }
 }
 
-impl From<serde_json::Error> for TransactionError 
-{
-    fn from(err: serde_json::Error) -> Self 
-    {
+impl From<serde_json::Error> for TransactionError {
+    fn from(err: serde_json::Error) -> Self {
         TransactionError::JsonError(err)
     }
 }
 
-impl From<std::io::Error> for TransactionError 
-{
-    fn from(err: std::io::Error) -> Self 
-    {
+impl From<std::io::Error> for TransactionError {
+    fn from(err: std::io::Error) -> Self {
         TransactionError::IoError(err)
     }
 }
 
-impl fmt::Display for TransactionError 
-{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result 
-    {
-        match self 
-        {
-            TransactionError::UreqError(e)    => write!(f, "HTTP Request Error: {}", e),
-            TransactionError::IoError(e)      => write!(f, "IO Error: {}", e),
-            TransactionError::JsonError(e)    => write!(f, "JSON Deserialization Error: {}", e),
+impl fmt::Display for TransactionError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            TransactionError::UreqError(e) => write!(f, "HTTP Request Error: {}", e),
+            TransactionError::IoError(e) => write!(f, "IO Error: {}", e),
+            TransactionError::JsonError(e) => write!(f, "JSON Deserialization Error: {}", e),
             TransactionError::ApiError(error) => write!(f, "Api error: {}", error),
         }
     }
